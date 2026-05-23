40dd47493814115a5a5d21b4d4049243b1c24765
Kartikey-Sharma108 <skartikeydjjdjj@gmail.com>
2026-05-23 21:37:36 +0530

    insertion sort

---
+++ Sorting/InsertionSortt.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class InsertionSortt {
+
+    public static void insertion(int[] arr){
+        for (int i = 1; i < arr.length ; i++) {
+            int j= i - 1;
+
+            int key = arr[i];
+
+            while (j>=0 && arr[j] > key ) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,44,67,5};
+       insertion(arr);
+       System.out.println(Arrays.toString(arr));
+    }
+}
